2919eef80e19ce84726806bb176dc5cf1a35beed
sinneren <sinneren@gmail.com>
2018-06-08 18:19:10 +0500

    Переименование

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import Auth from '../../containers/Auth';
+import Auth from '../../components/Auth';
 import * as AuthActions from '../../actions/AuthActions';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
-    }
     handleRootSubmit(data) {
         this.props.actions.login({
             email: data.email,
